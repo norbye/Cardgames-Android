78350a8a2fc97c2eec4d05542627927f82c3b71e
Jonatan Norbye <jonatan@norbye.com>
2017-02-28 22:30:17 +0100

    Initiated project

---
+++ app/src/main/java/com/norbye/dev/cardgames/MainActivity.java
@@ -0,0 +1,125 @@
+package com.norbye.dev.cardgames;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Action when floating button is clicked
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //Setup listview adapter
+        final ListView listview = (ListView) findViewById(R.id.game_list);
+        String[] values = new String[] { "1,Kontinental", "2,Amerikaner", "3,Janif", "4,Spardam" };
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final CustomArrayAdapter adapter = new CustomArrayAdapter(this,
+                values);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                //Send user to a view where it can start a game
+                Toast.makeText(getApplicationContext(), "Clicked" + view.getTag(), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    public class CustomArrayAdapter extends ArrayAdapter<String> {
+        private final Context context;
+        private final String[] values;
+
+        public CustomArrayAdapter(Context context, String[] values) {
+            super(context, -1, values);
+            this.context = context;
+            this.values = values;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.game_list_1, parent, false);
+            //Separate ID and title
+            String[] split = values[position].split(",", 2);
+            int id = Integer.parseInt(split[0]);
+            String text = split[1];
+            TextView textView = (TextView) rowView.findViewById(R.id.list_first_line);
+            //ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+            textView.setText(text);
+            rowView.setTag(id);
+            // change the icon for Windows and iPhone
+            /*String s = values[position];
+            if (s.startsWith("iPhone")) {
+                imageView.setImageResource(R.drawable.no);
+            } else {
+                imageView.setImageResource(R.drawable.ok);
+            }*/
+
+            return rowView;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
